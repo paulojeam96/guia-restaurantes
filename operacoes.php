ca4ea399c921024ffdad249a5a4ad82f4a01aa35
31448471 <31448471@087871-P33S105.macklabs.br>
2014-09-04 12:05:49 -0300

    exercicio 5.2

---
+++ operacoes.php
@@ -0,0 +1,17 @@
+<?php
+    function soma($x, $y){
+        return $x + $y;
+    }
+    
+    function subtracao($x, $y){
+        return $x-$y;
+    }
+    
+    function multiplicacao($x, $y){
+        return $x*$y;
+    }
+    
+    function divisao($x, $y){
+        return $x/$y;
+    }
+?>
---
+++ teste.php
@@ -0,0 +1,27 @@
+
+
+<?php
+include "operacoes.php";
+
+$x=2;
+$y=2;
+$soma= soma($x, $y);
+$subtracao = subtracao($x, $y);
+$multiplicacao= multiplicacao($x, $y);
+$divisao= divisao($x, $y);
+
+echo "soma"+ $soma;
+echo "</br>";
+echo "subtração" +$subtracao;
+echo "</br>";
+echo "multiplicação"+ $multiplicacao;
+echo "</br>";
+echo "divisão"+ $divisao;
+echo "</br>";
+
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+?>
